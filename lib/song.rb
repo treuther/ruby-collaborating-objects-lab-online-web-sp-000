60cca6a14262ab16b50589f467f5365f4febdd93
Tracy Reuther <tracyreuther@gmail.com>
2019-06-29 20:21:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,10 @@ class Song
     @name = name
   end
 
-  def self.new_by_filename(filename)
-
+  def self.new_by_filename(file_name)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
   end
 
 end
