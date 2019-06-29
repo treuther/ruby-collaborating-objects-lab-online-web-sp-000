cf9bff0e54100a775dd2258db56b670ea07482b6
Tracy Reuther <tracyreuther@gmail.com>
2019-06-29 19:25:53 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,8 @@
 class MP3Importer
 
+  def initialize
+
 def import(list_of_filenames)
-  list_of_filenames.each do |filename|
-    Song.new_by_filename(filename)
+  list_of_filenames.each {|filename| Song.new_by_filename(filename)}
   end
-end
 end
