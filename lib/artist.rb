2a2642e96118f2dcdded64d7e707404870dbd871
Tracy Reuther <tracyreuther@gmail.com>
2019-06-29 23:31:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,14 @@ def self.all
 end
 
 def self.find_or_create_by_name(artist_name)
-
+  found_artist = self.all.find{|artist| artist.name == artist_name}
+  if found_artist
+    found_artist
+  else
+    new_artist = self.new(artist_name)
+    new_artist.save
+    new_artist
+  end
 end
 
 def print_songs
